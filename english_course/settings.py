a5ae4256d46544b2d258a252e5566586bb48e58f
Cleilton Rodrigues <cleiltonrpj@gmail.com>
2026-02-12 18:34:37 -0300

    feat: restore Lesson 4 with interactive activities

--- english_course/settings.py
+++ english_course/settings.py
@@ -150,6 +150,10 @@ STATICFILES_DIRS = [BASE_DIR / 'core/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Media files (User uploaded)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
